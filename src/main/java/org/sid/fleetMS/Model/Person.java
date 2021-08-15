f5dcbefb2e55f931c20715a489cbb03de5672bb7
AwatefFaidi <feidiawatef@gmail.com>
2021-08-15 19:56:25 +0400

    first commit

---
+++ src/main/java/org/sid/fleetMS/Model/Person.java
@@ -0,0 +1,5 @@
+package org.sid.fleetMS.Model;
+
+public class Person {
+
+}
---
+++ src/main/java/org/sid/fleetMS/Model/State.java
@@ -0,0 +1,38 @@
+package org.sid.fleetMS.Model;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class State {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;	
+	private String name;
+	private String capital;
+    private String code;
+
+	@ManyToOne
+	@JoinColumn(name="countryid", insertable=false, updatable=false)
+	private Country country;
+	
+	private Integer countryid;
+	
+	private String details;
+
+}
